53a54809e0556fa0212ed239e647795626eadac9
Cyril Kocab <cyril.kocab@keyrus.com>
2025-03-31 14:09:45 +0000

    Source references, tests and documentation generation all working great !

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+select
+  id as payment_id,
+  orderid as order_id,
+  paymentmethod as payment_method,
+  status,
+  amount / 100 as amount, -- amount is stored in cents : converting to dollars
+  created as created_at
+from
+  {{ source('stripe', 'payment') }}
+--   dbt-tutorial.stripe.payment
